245ba9c18f0490984b058016491a3cdbfcc6f358
taherkhan30 <taherkhan@live.co.uk>
2023-01-31 20:41:05 +0000

    initial commit

---
+++ MvcApp/Data/ProductsContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcApp.Models
+{
+    public class ProductsContext : DbContext
+    {
+        public ProductsContext (DbContextOptions<ProductsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcApp.Models.Product> Product { get; set; } = default!;
+    }
+}
